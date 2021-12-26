d8777c326c1d1cf32e4bc3cc2649813c33af77d5
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-12-26 18:25:00 +0200

    feat: add make file for tests

---
+++ tests/loader_test.go
@@ -0,0 +1,16 @@
+package loader_test
+
+import (
+	"testing"
+
+	"github.com/ghaliesh/slouchwatch/loader"
+)
+
+func TestLoad(t *testing.T) {
+	var fileLoaer loader.EnvFileLoader = loader.NewDotEnvFileLoader()
+	actual := fileLoaer.Load("../.env")
+
+	if actual != nil {
+		t.Fatalf("FAILED: expected error to be %v but got: %v", nil, actual)
+	}
+}
